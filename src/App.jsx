dec716e7ae043202df4c74a5c985ba3d681541a3
Mert Araz <mertoreyiz1907@gmail.com>
2024-05-22 13:08:28 +0300

    add to react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import "./App.css";
-import Sidebar from "./components/Sidebar";
-import Dashboard from "./components/Dashboard";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CustomerList from "./components/pages/CustomerList";
+import Dashboard from "./components/pages/Dashboard";
+import Sidebar from "./components/common/Sidebar";
+
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 p-10 text-2xl font-bold">
-        <Dashboard />
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 p-10 text-2xl font-bold">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/customers" element={<CustomerList />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
