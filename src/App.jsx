18cac6633b924eabd13f334ad40539e134d39b90
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-06-03 01:01:03 +0300

    Refactor routes and add new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CustomerList from "./components/pages/CustomerList";
-import Dashboard from "./components/pages/Dashboard";
-import Sidebar from "./components/common/Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/common/Navbar";
+import Home from "./pages/Home";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="flex">
-        <Sidebar />
-        <div className="flex-1 p-10 text-2xl font-bold">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/customers" element={<CustomerList />} />
-          </Routes>
-        </div>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add other routes here */}
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
